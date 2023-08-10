8c158581a27b76a89bb1cdf78f261d40afe7e9a3
Anja Siewert <anja.siewert@live.de>
2023-08-10 19:47:13 +0200

    format date

--- blogposts/components/Blogposts/index.js
+++ blogposts/components/Blogposts/index.js
@@ -1,3 +1,8 @@
+function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: 'numeric', second: 'numeric'};
+    return new Date(dateString).toLocaleDateString('en-US', options);
+}
+
 export default function Blogposts({ posts }) {
     return (
         <>
@@ -5,7 +10,7 @@ export default function Blogposts({ posts }) {
             {posts.map((post) => (
             <li key={post.id}>
                 <h2>{post.title}</h2>
-                <p>created on: {post.created}</p>
+                <p>created on: {formatDate(post.created)}</p>
                 <p>Author: {post.author}</p>
                 </li>
             ))}
