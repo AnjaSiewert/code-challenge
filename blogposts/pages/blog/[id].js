1c059b6269327535e214162761700cad57cd2838
Anja Siewert <anja.siewert@live.de>
2023-08-11 12:37:52 +0200

    add back button

--- blogposts/pages/blog/[id].js
+++ blogposts/pages/blog/[id].js
@@ -1,12 +1,13 @@
 import Header from "@/components/Header";
+import Link from "next/link";
 
 export default function BlogpostDetailsPage ({selectedPost, formatDate}) {
     if(!selectedPost) {
         return <h2>is loading...</h2>
-    }
-
+    } 
     return (
         <>
+        <Link href="/">Back to Blog</Link>
         <Header headline={selectedPost.title}/>
         <p>subtitle: {selectedPost.subtitle}</p>
         <img src={selectedPost.image} alt={selectedPost.title}/>
