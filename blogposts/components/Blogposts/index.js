08aac63122a158000184a596a2ed333a459cc2c4
Anja Siewert <anja.siewert@live.de>
2023-08-11 12:55:50 +0200

    add BlogpostDetails component

--- blogposts/components/Blogposts/index.js
+++ blogposts/components/Blogposts/index.js
@@ -6,10 +6,10 @@ export default function Blogposts({ posts, formatDate }) {
         <ul> 
             {posts.map((post) => (
                 <Link href={`/blog/${post.id}`}>
-            <li key={post.id}>
-                <h2>{post.title}</h2>
-                <p>created on: {formatDate(post.created)}</p>
-                <p>Author: {post.author}</p>
+                <li key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>created on: {formatDate(post.created)}</p>
+                    <p>Author: {post.author}</p>
                 </li>
                 </Link>
             ))}
--- blogposts/pages/blog/[id].js
+++ blogposts/pages/blog/[id].js
@@ -1,19 +1,13 @@
+import BlogpostDetails from "@/components/BlogpostDetails";
 import Header from "@/components/Header";
 import Link from "next/link";
 
 export default function BlogpostDetailsPage ({selectedPost, formatDate}) {
-    if(!selectedPost) {
-        return <h2>is loading...</h2>
-    } 
     return (
         <>
         <Link href="/">Back to Blog</Link>
         <Header headline={selectedPost.title}/>
-        <p>subtitle: {selectedPost.subtitle}</p>
-        <img src={selectedPost.image} alt={selectedPost.title}/>
-        <p>created: {formatDate(selectedPost.created)}</p>
-        <p>{selectedPost.content}</p>
-        <p>Author: {selectedPost.author}</p>
+        <BlogpostDetails selectedPost={selectedPost} formatDate={formatDate}/>
         </>
     )
 };
