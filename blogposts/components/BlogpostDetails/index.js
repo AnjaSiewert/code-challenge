e8de01b22665a02eac48d4712696d4ca7fc0e0e1
Anja Siewert <anja.siewert@live.de>
2023-08-13 21:58:12 +0200

    create component ShareButtons

--- blogposts/components/BlogpostDetails/index.js
+++ blogposts/components/BlogpostDetails/index.js
@@ -1,28 +1,9 @@
-import { useState } from "react";
+import ShareButtons from "../ShareButtons";
 
 export default function BlogpostDetails ({selectedPost, formatDate}) {
     if(!selectedPost) {
         return <h2>is loading...</h2>
     } 
-
-    const [isCopied, setIsCopied] = useState(false);
-
-    const shareUrl = `${window.location.origin}/blog/${selectedPost.id}`;   
-    const shareText = `Check out this blogpost: ${selectedPost.title}`;
-
-function handleShareLink() {
-    navigator.clipboard.writeText(shareUrl);
-    setIsCopied(true);
-    setTimeout(() => { setIsCopied(false)}, 3000);
-};
-
-function handleShareWhatsApp() { 
-    window.open(`whatsapp://send?text=${encodeURIComponent(`${shareText} ${shareUrl}`)}`);
-};
-
-function handleShareMail() {
-    window.open(`mailto:?subject=${encodeURIComponent(shareText)}&body=${encodeURIComponent(shareUrl)}`);
-};
 
     return (
         <>
@@ -31,10 +12,7 @@ function handleShareMail() {
             <p>created: {formatDate(selectedPost.created)}</p>
             <p>{selectedPost.content}</p>
             <p>Author: {selectedPost.author}</p>
-            {isCopied && <p>Link copied to clipboard</p>}  
-            <button onClick={handleShareLink}>Copy link to clipboard</button>
-            <button onClick={handleShareWhatsApp}>Share via Whatsapp</button>
-            <button onClick={handleShareMail}>Share via Mail</button>
+            <ShareButtons selectedPost={selectedPost}/>
         </>
     );
 };
