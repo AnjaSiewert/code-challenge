953f522b77ad31638fbd74650d6ab541ed7d862d
Anja Siewert <anja.siewert@live.de>
2023-08-13 18:59:31 +0200

    remove console.log

--- blogposts/pages/_app.js
+++ blogposts/pages/_app.js
@@ -7,7 +7,6 @@ export default function App({ Component, pageProps }) {
  
   const router = useRouter();
   const { id } = router.query;
-  // console.log("router.query:",router.query);
 
   const { data, error, isLoading } = useSWR(
     id 
