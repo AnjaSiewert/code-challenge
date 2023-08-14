a339ff63313a8ad18652055ecd5fa8cd72f05752
Anja Siewert <anja.siewert@live.de>
2023-08-14 09:49:47 +0200

    center image with styled components

--- blogposts/components/BlogpostDetails/index.js
+++ blogposts/components/BlogpostDetails/index.js
@@ -1,4 +1,13 @@
+import styled from 'styled-components';
 import ShareButtons from "../ShareButtons";
+
+const StyledImage = styled.img`
+    width: 50%;`;
+
+const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;`;
 
 export default function BlogpostDetails ({selectedPost, formatDate}) {
     if(!selectedPost) {
@@ -8,7 +17,9 @@ export default function BlogpostDetails ({selectedPost, formatDate}) {
     return (
         <>
             <p>subtitle: {selectedPost.subtitle}</p>
-            <img src={selectedPost.image} alt={selectedPost.title}/>
+            <StyledDiv>
+            <StyledImage src={selectedPost.image} alt={selectedPost.title}/>
+            </StyledDiv>
             <p>created: {formatDate(selectedPost.created)}</p>
             <p>{selectedPost.content}</p>
             <p>Author: {selectedPost.author}</p>
