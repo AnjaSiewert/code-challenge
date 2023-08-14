181bfa351effa6b4a32488a49a9727cb1337ab58
Anja Siewert <anja.siewert@live.de>
2023-08-14 13:41:04 +0200

    add parapraphs on homepage

--- blogposts/components/Blogposts/index.js
+++ blogposts/components/Blogposts/index.js
@@ -8,8 +8,8 @@ export default function Blogposts({ posts, formatDate }) {
                 <Link href={`/blog/${post.id}`}>
                 <li key={post.id}>
                     <h2>{post.title}</h2>
-                    <p>created on: {formatDate(post.created)}</p>
-                    <p>Author: {post.author}</p>
+                    <p><strong>Created on: </strong><br/>{formatDate(post.created)}</p>
+                    <p><strong>Author: </strong><br />{post.author}</p>
                 </li>
                 </Link>
             ))}
