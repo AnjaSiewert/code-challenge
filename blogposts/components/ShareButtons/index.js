944447d03f7becfd74e43a475bad3d8c7dd7d8de
Anja Siewert <anja.siewert@live.de>
2023-08-14 10:51:33 +0200

    style share buttons

--- blogposts/components/ShareButtons/index.js
+++ blogposts/components/ShareButtons/index.js
@@ -1,4 +1,22 @@
 import { useState } from "react";
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    cursor: pointer;
+    font-size: 0.8rem;
+    height: 2rem;
+    border: none;
+    border-radius: 10px;
+    color: white;
+    background: #333;
+    padding: 0.5rem 1rem;
+    box-shadow: 0 0.2rem rgba(121,121,121, 0.65);
+    
+    &:hover {
+        filter: brightness(130%);
+        transform: scale(1.05);
+    }
+    `;
 
 export default function ShareButtons ({selectedPost}) {
     const [isCopied, setIsCopied] = useState(false);
@@ -23,9 +41,9 @@ function handleShareMail() {
 return (
     <>  
        {isCopied && <p>Link copied to clipboard</p>}  
-            <button onClick={handleShareLink}>Copy link to clipboard</button>
-            <button onClick={handleShareWhatsApp}>Share via Whatsapp</button>
-            <button onClick={handleShareMail}>Share via Mail</button>
+            <StyledButton onClick={handleShareLink}>Copy link to clipboard</StyledButton>
+            <StyledButton onClick={handleShareWhatsApp}>Share via Whatsapp</StyledButton>
+            <StyledButton onClick={handleShareMail}>Share via Mail</StyledButton>
     </>
 );
 };
