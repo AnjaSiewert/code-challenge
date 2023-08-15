eff619e3d65179ed8fd0e4d71f0274b1319315e0
Anja Siewert <anja.siewert@live.de>
2023-08-15 10:08:38 +0200

    add handler function

---
+++ blogposts/pages/api/blogposts/index.js
@@ -0,0 +1,9 @@
+import { blogposts } from "../data";
+
+export default function handler(req, res) {
+    if(req.method !== 'GET') {
+        res.status(200).json(blogposts);
+    } else {
+        res.status(400).json({message: 'Only GET requests are allowed'});
+    }  
+};
